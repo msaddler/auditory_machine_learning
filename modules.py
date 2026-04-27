b86302e1b7289eed03a17da2f644a6027af992d7
Mark Saddler <msaddler@mit.edu>
2026-04-27 19:15:26 +0200

    modifications to run cochlear model and components on numpy / non-batched inputs

--- modules.py
+++ modules.py
@@ -5,6 +5,86 @@ import filters
 import utils
 
 
+class CochlearModel(torch.nn.Module):
+    def __init__(
+        self,
+        sr_input=20000,
+        sr_output=10000,
+        fir_dur=0.05,
+        cfs=utils.erbspace(8e1, 8e3, 100),
+        bw_mult=1.0,
+        ihc_lowpass_cutoff=3000,
+        ihc_lowpass_order=7,
+        threshold=0.0,
+        dynamic_range=80.0,
+        dtype=torch.float32,
+    ):
+        """
+        Simple cochlear model in PyTorch.
+
+        Args
+        ----
+        sr_input (int): sampling rate of the input sound waveform
+        sr_output (int): sampling rate of the output representation
+        fir_dur (float): duration of finite impulse responses for filtering (s)
+        cfs (np.ndarray): characteristic frequencies of the cochlear filters (Hz)
+        bw_mult (float): scaling factor applied to cochlear filter bandwidths
+        ihc_lowpass_cutoff (float): cutoff frequency of IHC lowpass filter (Hz)
+        ihc_lowpass_order (float): order of IHC lowpass filter
+        threshold (float): absolute threshold of auditory nerve fibers (dB SPL)
+        dynamic_range (float): auditory nerve fiber dynamic range (dB)
+        dtype (torch.dtype): datatype for internal intensors and inputs
+        """
+        super().__init__()
+        self.sr_input = sr_input
+        self.sr_output = sr_output
+        self.cfs = cfs
+        self.dtype = dtype
+        self.cochlear_filterbank = GammatoneFilterbank(
+            sr=sr_input,
+            fir_dur=fir_dur,
+            cfs=self.cfs,
+            dtype=dtype,
+            bw_mult=bw_mult,
+        )
+        self.half_wave_rectification = torch.nn.ReLU()
+        self.ihc_lowpass_filter = IHCLowpassFilter(
+            sr_input=sr_input,
+            sr_output=sr_output,
+            fir_dur=fir_dur,
+            cutoff=ihc_lowpass_cutoff,
+            order=ihc_lowpass_order,
+        )
+        self.rate_level_function = SigmoidRateLevelFunction(
+            rate_spont=0.0,
+            rate_max=250.0,
+            threshold=threshold,
+            dynamic_range=dynamic_range,
+            dynamic_range_interval=0.95,
+            dtype=dtype,
+        )
+
+    def forward(self, x):
+        """
+        Run the cochlear model on input sound waveform(s).
+
+        Args
+        ----
+        x (torch.Tensor): input sound waveform(s) with shape [batch, time] or [time]
+
+        Returns
+        -------
+        x (torch.Tensor): output with shape [batch, freq, time] or [freq, time]
+        """
+        x = torch.as_tensor(x, dtype=self.dtype)
+        x = self.cochlear_filterbank(x)
+        x = self.half_wave_rectification(x)
+        x = self.ihc_lowpass_filter(x)
+        x = self.half_wave_rectification(x)
+        x = self.rate_level_function(x)
+        return x
+
+
 class FIRFilterbank(torch.nn.Module):
     def __init__(self, fir, dtype=torch.float32, **kwargs_conv1d):
         """
@@ -21,12 +101,10 @@ class FIRFilterbank(torch.nn.Module):
             which is used for batching)
         """
         super().__init__()
-        if not isinstance(fir, (list, np.ndarray, torch.Tensor)):
-            raise TypeError(f"unrecognized {type(fir)=}")
-        if isinstance(fir, (list, np.ndarray)):
-            fir = torch.tensor(fir, dtype=dtype)
+        fir = torch.as_tensor(fir, dtype=dtype)
         if fir.ndim not in [1, 2]:
-            raise ValueError(f"invalid {fir.shape=}")
+            msg = f"{fir.shape=} must be [n_taps] or [n_filters, n_taps]"
+            raise ValueError(msg)
         self.register_buffer("fir", fir)
         self.kwargs_conv1d = kwargs_conv1d
 
@@ -37,32 +115,36 @@ class FIRFilterbank(torch.nn.Module):
 
         Args
         ----
-        x (torch.Tensor): input signal
+        x (torch.Tensor): input signal with shape [..., time]
         batching (bool): if True, the input is assumed to have shape
             [..., n_filters, time] and each channel is filtered with
             its own filter
 
         Returns
         -------
-        y (torch.Tensor): filtered signal
+        x (torch.Tensor): filtered signal
         """
-        y = x
+        no_batch_dim = x.ndim == 1
+        if no_batch_dim:
+            x = x[None, ...]
         if batching:
-            assert y.shape[-2] == self.fir.shape[0]
+            assert x.shape[-2] == self.fir.shape[0]
         else:
-            y = y.unsqueeze(-2)
-        unflatten_shape = y.shape[:-2]
-        y = torch.flatten(y, start_dim=0, end_dim=-2 - 1)
-        y = torch.nn.functional.conv1d(
-            input=torch.nn.functional.pad(y, (self.fir.shape[-1] - 1, 0)),
+            x = x.unsqueeze(-2)
+        unflatten_shape = x.shape[:-2]
+        x = torch.flatten(x, start_dim=0, end_dim=-2 - 1)
+        x = torch.nn.functional.conv1d(
+            input=torch.nn.functional.pad(x, (self.fir.shape[-1] - 1, 0)),
             weight=self.fir.flip(-1).view(-1, 1, self.fir.shape[-1]),
             **self.kwargs_conv1d,
-            groups=y.shape[-2] if batching else 1,
+            groups=x.shape[-2] if batching else 1,
         )
-        y = torch.unflatten(y, 0, unflatten_shape)
+        x = torch.unflatten(x, 0, unflatten_shape)
         if self.fir.ndim == 1:
-            y = y.squeeze(-2)
-        return y
+            x = x.squeeze(-2)
+        if no_batch_dim:
+            x = x[0]
+        return x
 
 
 class GammatoneFilterbank(FIRFilterbank):
@@ -111,6 +193,98 @@ class IHCLowpassFilter(FIRFilterbank):
         msg = f"{sr_input=} and {sr_output=} require non-integer stride"
         assert np.isclose(stride, sr_input / sr_output), msg
         super().__init__(fir, dtype=dtype, stride=stride)
+
+
+class SigmoidRateLevelFunction(torch.nn.Module):
+    def __init__(
+        self,
+        rate_spont=0.0,
+        rate_max=250.0,
+        threshold=0.0,
+        dynamic_range=80.0,
+        dynamic_range_interval=0.95,
+        dtype=torch.float32,
+    ):
+        """
+        Sigmoid function to convert sound pressure in Pa to auditory nerve firing
+        rates in spikes per second. This function crudely accounts for loss of
+        audibility at low sound levels and saturation at high sound levels.
+
+        Args
+        ----
+        rate_spont (float): spontaneous firing rate in spikes/s
+        rate_max (float): maximum firing rate in spikes/s
+        threshold (float): auditory nerve fiber threshold for spiking (dB SPL)
+        dynamic_range (float): dynamic range over which firing rate changes (dB)
+        dynamic_range_interval (float): determines proportion of firing rate change
+            within dynamic_range (default is 95%)
+        dtype (torch.dtype): data type for inputs and internal tensors
+        """
+        super().__init__()
+        self.rate_spont = torch.as_tensor(rate_spont, dtype=dtype)
+        self.rate_max = torch.as_tensor(rate_max, dtype=dtype)
+        self.threshold = torch.as_tensor(threshold, dtype=dtype)
+        self.dynamic_range = torch.as_tensor(dynamic_range, dtype=dtype)
+        y_threshold = torch.as_tensor(
+            (1 - dynamic_range_interval) / 2,
+            dtype=dtype,
+        )
+        self.register_buffer(
+            "k",
+            torch.log((1 / y_threshold) - 1) / (self.dynamic_range / 2),
+        )
+        self.register_buffer(
+            "x0",
+            self.threshold - (torch.log((1 / y_threshold) - 1) / (-self.k)),
+        )
+
+    def forward(self, x):
+        """
+        Apply sigmoid auditory nerve rate-level function.
+
+        Args
+        ----
+        x (torch.Tensor): half-wave rectified and low-pass filtered subbands with
+            shape [..., freq, time]
+
+        Returns
+        -------
+        x (torch.Tensor): instantaneous firing rates with shape [..., freq, time]
+        """
+        input_ndim = x.ndim
+        while x.ndim < 3:
+            x = x.unsqueeze(-2)
+        assert x.ndim == 3, "expected input with shape [batch, freq, time]"
+        x = 20 * torch.log10(x / 20e-6)
+        x = GradientStableSigmoid.apply(
+            x,
+            self.k.view(1, -1, 1),
+            self.x0.view(1, -1, 1),
+        )
+        x = self.rate_spont + (self.rate_max - self.rate_spont) * x
+        while x.ndim > input_ndim:
+            x = x.squeeze(-2)
+        return x
+
+
+class GradientStableSigmoid(torch.autograd.Function):
+    """
+    Custom autograd Function for sigmoid function with stable
+    gradient (avoid NaN due to overflow in rate-level function).
+    """
+
+    @staticmethod
+    def forward(ctx, x, k, x0):
+        ctx.save_for_backward(x, k, x0)
+        return 1.0 / (1.0 + torch.exp(-k * (x - x0)))
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        x, k, x0 = ctx.saved_tensors
+        grad = k * torch.exp(-k * (x - x0))
+        grad = grad / (torch.exp(-k * (x - x0)) + 1.0) ** 2
+        grad = torch.nan_to_num(grad, nan=0.0, posinf=None, neginf=None)
+        return grad_output * grad, None, None
 
 
 class AudioConv1d(torch.nn.Conv1d):
@@ -191,7 +365,7 @@ class HalfCosineFilterbank(torch.nn.Module):
         cf_low=10e0,
         cf_high=10e3,
         cf_num=50,
-        scale="erb",
+        scale="log",
         include_highpass=False,
         include_lowpass=False,
     ):
@@ -244,6 +418,13 @@ class HalfCosineFilterbank(torch.nn.Module):
                 num=self.cf_num,
             )
             self.cfs = utils.erb2freq(cfs)
+        elif self.scale.lower() == "linear":
+            cfs = np.linspace(
+                start=self.cf_low,
+                stop=self.cf_high,
+                num=self.cf_num,
+            )
+            self.cfs = cfs
         elif self.scale.lower() == "log":
             f = np.log(f)
             cfs = np.linspace(
@@ -252,13 +433,6 @@ class HalfCosineFilterbank(torch.nn.Module):
                 num=self.cf_num,
             )
             self.cfs = np.exp(cfs)
-        elif self.scale.lower() == "linear":
-            cfs = np.linspace(
-                start=self.cf_low,
-                stop=self.cf_high,
-                num=self.cf_num,
-            )
-            self.cfs = cfs
         else:
             raise ValueError(f"unrecognized filterbank scale: {self.scale}")
         bw = 2 * (cfs[1] - cfs[0]) if self.cf_num > 1 else np.inf
@@ -275,113 +449,24 @@ class HalfCosineFilterbank(torch.nn.Module):
 
     def forward(self, x):
         """
-        Apply filterbank along time axis (dim=1) in the frequency domain.
+        Apply filterbank along time axis (dim=-1) in the frequency domain.
         Construct new filterbank on first call or if input shape changes.
         """
-        assert x.ndim >= 2, "Expected input shape [batch, time, ...]"
-        y = torch.fft.rfft(x, dim=1)
-        y = torch.unsqueeze(y, dim=1)
+        y = torch.fft.rfft(x, dim=-1)
+        y = torch.unsqueeze(y, dim=-2)
         rebuild = self.filters is None
         rebuild = rebuild or (not y.device == self.filters.device)
         rebuild = rebuild or (not y.ndim == self.filters.ndim)
-        rebuild = rebuild or (not y.shape[2] == self.filters.shape[2])
+        rebuild = rebuild or (not y.shape[-1] == self.filters.shape[-1])
         if rebuild:
-            f = np.fft.rfftfreq(x.shape[1], d=1 / self.sr)
+            f = np.fft.rfftfreq(x.shape[-1], d=1 / self.sr)
             self.filters = torch.as_tensor(
                 self.get_frequency_domain_filters(f),
                 dtype=y.dtype,
                 device=x.device,
             )
-            self.filters = self.filters[None, ...]
-            for _ in range(2, x.ndim):
-                self.filters = self.filters[..., None]
+            while self.filters.ndim < y.ndim:
+                self.filters = self.filters[None, ...]
         y = y * self.filters
-        y = torch.fft.irfft(y, dim=2)
+        y = torch.fft.irfft(y, dim=-1)
         return y
-
-
-class GradientStableSigmoid(torch.autograd.Function):
-    """
-    Custom autograd Function for sigmoid function with stable
-    gradient (avoid NaN due to overflow in rate-level function).
-    """
-
-    @staticmethod
-    def forward(ctx, x, k, x0):
-        ctx.save_for_backward(x, k, x0)
-        return 1.0 / (1.0 + torch.exp(-k * (x - x0)))
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        x, k, x0 = ctx.saved_tensors
-        grad = k * torch.exp(-k * (x - x0))
-        grad = grad / (torch.exp(-k * (x - x0)) + 1.0) ** 2
-        grad = torch.nan_to_num(grad, nan=0.0, posinf=None, neginf=None)
-        return grad_output * grad, None, None
-
-
-class SigmoidRateLevelFunction(torch.nn.Module):
-    def __init__(
-        self,
-        rate_spont=0.0,
-        rate_max=250.0,
-        threshold=0.0,
-        dynamic_range=80.0,
-        dynamic_range_interval=0.95,
-        dtype=torch.float32,
-    ):
-        """
-        Sigmoid function to convert sound pressure in Pa to auditory nerve firing
-        rates in spikes per second. This function crudely accounts for loss of
-        audibility at low sound levels and saturation at high sound levels.
-
-        Args
-        ----
-        rate_spont (float): spontaneous firing rate in spikes/s
-        rate_max (float): maximum firing rate in spikes/s
-        threshold (float): auditory nerve fiber threshold for spiking (dB SPL)
-        dynamic_range (float): dynamic range over which firing rate changes (dB)
-        dynamic_range_interval (float): determines proportion of firing rate change
-            within dynamic_range (default is 95%)
-        dtype (torch.dtype): data type for inputs and internal tensors
-        """
-        super().__init__()
-        self.rate_spont = torch.as_tensor(rate_spont, dtype=dtype)
-        self.rate_max = torch.as_tensor(rate_max, dtype=dtype)
-        self.threshold = torch.as_tensor(threshold, dtype=dtype)
-        self.dynamic_range = torch.as_tensor(dynamic_range, dtype=dtype)
-        y_threshold = torch.as_tensor(
-            (1 - dynamic_range_interval) / 2,
-            dtype=dtype,
-        )
-        self.register_buffer(
-            "k",
-            torch.log((1 / y_threshold) - 1) / (self.dynamic_range / 2),
-        )
-        self.register_buffer(
-            "x0",
-            self.threshold - (torch.log((1 / y_threshold) - 1) / (-self.k)),
-        )
-
-    def forward(self, x):
-        """
-        Apply sigmoid auditory nerve rate-level function.
-
-        Args
-        ----
-        x (torch.Tensor): half-wave rectified and low-pass filtered subbands with
-            shape [batch, freq, time]
-
-        Returns
-        -------
-        x (torch.Tensor): instantaneous firing rates with shape [batch, freq, time]
-        """
-        assert x.ndim == 3, "expected input with shape [batch, freq, time]"
-        x = 20.0 * torch.log10(x / 20e-6)
-        x = GradientStableSigmoid.apply(
-            x,
-            self.k.view(1, -1, 1),
-            self.x0.view(1, -1, 1),
-        )
-        x = self.rate_spont + (self.rate_max - self.rate_spont) * x
-        return x
